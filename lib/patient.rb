a6996d8161378a68337eb7c4f829ac9ae8ecc646
Javier Aguilar <coyote176@msn.com>
2020-08-27 02:27:41 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,7 +12,19 @@ class Patient
     @@all
   end
   
-  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
 
 
 end
