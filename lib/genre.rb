b4cf006076639acca4b4a5b43eb8059b72272a27
Javier Aguilar <coyote176@msn.com>
2020-08-27 00:58:13 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,12 +1,13 @@
 class Genre 
 
-attr_accessor :name :songs :
+attr_accessor :name, :songs, :artists
 
   @@all= []
   
-  def initialize(name)
+  def initialize(name, songs, artists)
     @name = name
-    
+    @songs= songs
+    @artists= artists
     @@all << self
   end
   
